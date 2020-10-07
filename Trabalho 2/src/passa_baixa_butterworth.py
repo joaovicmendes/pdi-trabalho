44a6ce8c4ec26801fab5c1bb427a5d3e2b2069ec
João Victor Mendes <mendesjoaovic@gmail.com>
2020-10-06 22:06:19 -0300

    Partes do texto reescritas e correção nos arquivos .py

--- Trabalho 2/src/passa_baixa_butterworth.py
+++ Trabalho 2/src/passa_baixa_butterworth.py
@@ -1,16 +1,13 @@
-def filtro_passa_alta_butterworth(img, d0, n):    
+def filtro_passa_baixa_butterworth(img, d0, n):
     num_rows, num_cols = img.shape   
     freq_r, freq_c = generate_frequencies(num_rows, num_cols)
     
-    high_pass_butterworth = np.zeros([num_rows, num_cols])
-    
+    low_pass_butterworth = np.zeros([num_rows, num_cols])
     for row in range(num_rows):
         for col in range(num_cols):
             dist = np.sqrt(freq_r[row]**2 + freq_c[col]**2)
-            if dist == 0:
-                dist = 1
-            H = 1/(1+(d0/dist)**(2*n))
+            H = 1/(1 + (dist/d0)**(2*n))
             
-            high_pass_butterworth[row,col] = H
+            low_pass_butterworth[row, col] = H
     
-    return high_pass_butterworth
+    return low_pass_butterworth
